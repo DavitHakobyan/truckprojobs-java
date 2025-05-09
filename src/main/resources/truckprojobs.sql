0a6b48653e226ce0f044116016065f2c09b03e77
Davit Hakobyan <hakobyan.david@gmail.com>
2025-05-09 13:15:15 -0700

    Add JobCompany, JobLocation, and JobPostActivity entities with corresponding SQL table definitions

--- src/main/resources/truckprojobs.sql
+++ src/main/resources/truckprojobs.sql
@@ -91,3 +91,27 @@ CREATE TABLE recruiter_profile
     PRIMARY KEY (user_account_id),
     CONSTRAINT fk_user_account FOREIGN KEY (user_account_id) REFERENCES users (user_id)
 );
+
+CREATE TABLE job_company
+(
+    id   SERIAL PRIMARY KEY,
+    logo VARCHAR(255),
+    name VARCHAR(255)
+);
+
+CREATE TABLE job_post_activity
+(
+    job_post_id        SERIAL PRIMARY KEY,
+    description_of_job VARCHAR(10000),
+    job_title          VARCHAR(255),
+    job_type           VARCHAR(255),
+    posted_date        TIMESTAMP(6),
+    remote             VARCHAR(255),
+    salary             VARCHAR(255),
+    job_company_id     INT,
+    job_location_id    INT,
+    posted_by_id       INT,
+    CONSTRAINT FKpjpv059hollr4tk92ms09s6is FOREIGN KEY (job_company_id) REFERENCES job_company (id),
+    CONSTRAINT FK44003mnvj29aiijhsc6ftsgxe FOREIGN KEY (job_location_id) REFERENCES job_location (id),
+    CONSTRAINT FK62yqqbypsq2ik34ngtlw4m9k3 FOREIGN KEY (posted_by_id) REFERENCES users (user_id)
+);
