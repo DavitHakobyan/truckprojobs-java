0dcff3cf8f4cb0b9c0947fcd5397fc102b22b604
Davit Hakobyan <hakobyan.david@gmail.com>
2025-05-09 23:08:01 -0700

    Enhance CandidateApplyController to manage job applications and saves; update JobPostActivity and Users models for improved database mapping; refactor services for user profile retrieval

--- src/main/java/com/smartit/truckprojobs/model/Users.java
+++ src/main/java/com/smartit/truckprojobs/model/Users.java
@@ -1,14 +1,10 @@
 package com.smartit.truckprojobs.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.ToString;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
-@Data
-@ToString
 @Entity
 @Table(name = "users")
 public class Users {
@@ -22,7 +18,7 @@ public class Users {
     private String email;
 
     @Column(name = "is_active")
-    private Boolean active;
+    private boolean active;
 
     @Column(name = "password")
     private String password;
@@ -49,4 +45,74 @@ public class Users {
         this.userTypeId = userTypeId;
     }
     // Getters and setters (optional)
+
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+//    public boolean isActive() {
+//        return isActive;
+//    }
+//
+//    public void setActive(boolean active) {
+//        isActive = active;
+//    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public UsersType getUserTypeId() {
+        return userTypeId;
+    }
+
+    public void setUserTypeId(UsersType userTypeId) {
+        this.userTypeId = userTypeId;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "userId=" + userId +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", isActive=" + active +
+                ", registrationDate=" + registrationDate +
+                ", userTypeId=" + userTypeId +
+                '}';
+    }
 }
--- src/main/java/com/smartit/truckprojobs/service/RecruiterProfileService.java
+++ src/main/java/com/smartit/truckprojobs/service/RecruiterProfileService.java
@@ -1,7 +1,13 @@
 package com.smartit.truckprojobs.service;
 
 import com.smartit.truckprojobs.model.RecruiterProfile;
+import com.smartit.truckprojobs.model.Users;
 import com.smartit.truckprojobs.repository.RecruiterProfileRepository;
+import com.smartit.truckprojobs.repository.UsersRepository;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -10,9 +16,12 @@ import java.util.Optional;
 public class RecruiterProfileService {
 
     private final RecruiterProfileRepository recruiterRepository;
+    private final UsersRepository usersRepository;
 
-    public RecruiterProfileService(RecruiterProfileRepository recruiterRepository) {
+    public RecruiterProfileService(RecruiterProfileRepository recruiterRepository,
+                                   UsersRepository usersRepository) {
         this.recruiterRepository = recruiterRepository;
+        this.usersRepository = usersRepository;
     }
 
     public Optional<RecruiterProfile> getOne(Long id) {
@@ -21,5 +30,16 @@ public class RecruiterProfileService {
 
     public RecruiterProfile addNew(RecruiterProfile recruiterProfile) {
         return recruiterRepository.save(recruiterProfile);
+    }
+
+    public RecruiterProfile getCurrentRecruiterProfile() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication instanceof AnonymousAuthenticationToken) {
+            return null;
+        }
+        String currentUsername = authentication.getName();
+        Users users = usersRepository.findByEmail(currentUsername).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        Optional<RecruiterProfile> recruiterProfile = getOne(users.getUserId());
+        return recruiterProfile.orElse(null);
     }
 }
